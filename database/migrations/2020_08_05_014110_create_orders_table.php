58ab6e0e02e019425098db251df8c6daa113d66e
Andrey Polyanskiy <polyanskiy@mail.ru>
2020-08-05 05:34:47 +0300

    initial commit

---
+++ database/migrations/2020_08_05_014110_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('number');
+            $table->timestamp('accepted_at')->nullable();
+            $table->string('title');
+            $table->text('text');
+            $table->string('contact_name');
+            $table->string('contact_phone');
+            $table->string('ati');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}

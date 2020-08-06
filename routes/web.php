e090f32feab0bfbba3fee720191ba44f6d69fd07
Andrey Polyanskiy <polyanskiy@mail.ru>
2020-08-06 09:35:09 +0300

    first iteration

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('orders', 'OrderController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
